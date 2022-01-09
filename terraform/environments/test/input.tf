2a1e550b1557ef9136b802a81c72f26cc80800ac
juniorcfaj <junior.cfaj@gmail.com>
2022-01-09 13:52:07 -0300

    agora

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -9,7 +9,8 @@ variable "location" {}
 variable "resource_group" {}
 variable "application_type" {}
 
-# Tags
-variable tier {}
-variable deployment {}
+# Network
+variable virtual_network_name {}
+variable address_prefix_test {}
+variable address_space {}
 
--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,16 +7,10 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
-  }
-}
-locals {
-  tags = {
-    tier        = "${var.tier}"
-    deployment  = "${var.deployment}"
+    storage_account_name = "tstate18893"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    access_key           = "UGUYGaB3TQ5HdoqtfirEA9NK6flOaki5g0OOdCp+1DFlNxc5h0QDchvJooCTfXcVGgegNbEMhdvouKfX2QidAw=="
   }
 }
 module "resource_group" {
@@ -24,11 +18,37 @@ module "resource_group" {
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
+module "network" {
+  source               = "../../modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${module.resource_group.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
+
+module "nsg-test" {
+  source           = "../../modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
 module "appservice" {
   source           = "../../modules/appservice"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
   resource_group   = "${module.resource_group.resource_group_name}"
-  tags             = "${local.tags}"
+}
+module "publicip" {
+  source           = "../../modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${module.resource_group.resource_group_name}"
 }

1979c5da5a6eb65b903b9bbdbc85957b508f203d
juniorcfaj <junior.cfaj@gmail.com>
2022-01-09 14:08:29 -0300

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -14,9 +14,9 @@ terraform {
   }
 }
 module "resource_group" {
-  source               = "../../modules/resource_group"
-  resource_group       = var.resource_group
-  location             = var.location
+  source         = "../../modules/resource_group"
+  resource_group = var.resource_group
+  location       = var.location
 }
 module "network" {
   source               = "../../modules/network"

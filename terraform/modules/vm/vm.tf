71ccaef19a390173ac46ec36cdb5be0b1cf8e5e1
juniorcfaj <junior.cfaj@gmail.com>
2022-01-09 16:13:56 -0300

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -5,9 +5,9 @@ resource "azurerm_network_interface" "" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
